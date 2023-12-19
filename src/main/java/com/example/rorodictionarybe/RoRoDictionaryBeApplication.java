570bb2b5d153fbdab3f872bce6f2ebbe5870f4a0
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-19 21:06:28 +0700

    first commit

---
+++ src/main/java/com/example/rorodictionarybe/RoRoDictionaryBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rorodictionarybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoRoDictionaryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoRoDictionaryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rorodictionarybe/RoRoDictionaryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rorodictionarybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoRoDictionaryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
