e26b009f26c3d0f29ee85b20d8a0882cc61a8efb
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-19 21:07:48 +0700

    change readme

--- src/main/java/com/example/rorodictionarybe/RoRoDictionaryBeApplication.java
+++ src/main/java/com/example/rorodictionarybe/RoRoDictionaryBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoRoDictionaryBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoRoDictionaryBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoRoDictionaryBeApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/rorodictionarybe/RoRoDictionaryBeApplicationTests.java
+++ src/test/java/com/example/rorodictionarybe/RoRoDictionaryBeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RoRoDictionaryBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
